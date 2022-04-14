7eb97847e4e9b6fe18dc14a3bf8cd4c908170709
Daniele Pellegrini <danny.m.pellegrini@gmail.com>
2022-04-14 14:17:02 -0400

    Initial Commit

---
+++ Lesson 2 GitHub and Xcode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lesson 2 GitHub and Xcode
+//
+//  Created by Daniele Pellegrini on 4/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lesson 2 GitHub and Xcode/Lesson_2_GitHub_and_XcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_2_GitHub_and_XcodeApp.swift
+//  Lesson 2 GitHub and Xcode
+//
+//  Created by Daniele Pellegrini on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_2_GitHub_and_XcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
